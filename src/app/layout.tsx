922eb4154bfc1bf204ad505aa5a5f9ef7f145556
Azan Baig <baigu@Azans-MacBook-Pro.local>
2025-11-21 00:20:50 -0500

    Fixing Title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "People&apos;s Voice",
+  title: "Peoples Voice",
   description: "Your city, your voice",
 };
 
